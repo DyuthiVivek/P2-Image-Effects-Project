457f86a37682864885c6da2e5a45b569d27b9c89
Akshaya8583 <akshaya@DESKTOP-J8UDRR0>
2023-12-29 17:52:35 +0530

    invert effect

--- ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/service/PhotoEffectService.java
+++ ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/service/PhotoEffectService.java
@@ -180,7 +180,8 @@ public class PhotoEffectService {
             // ACTUAL WORK STARTS HERE
 
             // TODO
-            Pixel[][] modifiedImage = inputImage; // Replace this with actual modified image
+	    InvertEffect effect = new InvertEffect();
+            Pixel[][] modifiedImage = effect.apply(inputImage, imageName, loggingService); // Replace this with actual modified image
 
             // ACTUAL WORK ENDS HERE
 
