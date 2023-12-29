a981cdf9a47def76cda09d99fc4d438dd574c8ab
DyuthiVivek <Dyuthi.Vivek@iiitb.ac.in>
2023-12-29 13:01:50 +0530

    clearlogs and get by effect

--- ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/service/LoggingService.java
+++ ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/service/LoggingService.java
@@ -1,25 +1,29 @@
 package com.iiitb.imageEffectApplication.service;
-import com.iiitb.imageEffectApplication.model.LogModel;
-import org.springframework.stereotype.Service;
-
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.io.*;
+
+import org.springframework.stereotype.Service;
+
+import com.iiitb.imageEffectApplication.model.LogModel;
 
 @Service
 public class LoggingService {
 
     public void addLog(String fileName, String effectName, String optionValues) {
-        String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new java.util.Date());
+        String timeStamp = new SimpleDateFormat("dd MM yyyy hh:mm").format(new java.util.Date());
+        System.out.println(timeStamp);
         LogModel logModel = new LogModel(timeStamp, fileName, effectName, optionValues);
+        List <LogModel> listOfLogs = getAllLogs();
         File file = new File("/home/dyuthi/prog2/P2-Image-Effects-Project/ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/service/logs");
+        
         try (FileOutputStream fos = new FileOutputStream(file)) {
             ObjectOutputStream oos = new ObjectOutputStream(fos);
+            for(LogModel log: listOfLogs)
+                oos.writeObject(log);
             oos.writeObject(logModel);
-            System.out.println("I wrote something");
             oos.close();
             fos.close();
         } catch (IOException e) {
@@ -47,17 +51,53 @@ public class LoggingService {
             ostream.close();
             fstream.close();
         }
-        catch(Exception e){
+        catch(EOFException e){
+            
+        }
+
+        catch(IOException | ClassNotFoundException e){
             e.printStackTrace();
         }        
         return listOfLogs;
     }
 
     public List<LogModel> getLogsByEffect(String effectName) {
-        return new ArrayList<LogModel>();
+        ArrayList <LogModel> listOfLogs = new ArrayList<>();
+        File file = new File("/home/dyuthi/prog2/P2-Image-Effects-Project/ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/service/logs");
+
+        try {
+            FileInputStream fstream = new FileInputStream(file);
+            ObjectInputStream ostream = new ObjectInputStream(fstream);
+            while (true) {
+                Object obj;
+                try {
+                    obj = ostream.readObject();
+                } catch (EOFException e) {
+                    break;
+                }
+
+                if(((LogModel)obj).getEffectName().equals(effectName))
+                    listOfLogs.add((LogModel)obj);
+            }
+            ostream.close();
+            fstream.close();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }        
+        return listOfLogs;
+
     }
 
     public void clearLogs() {
+        File file = new File("/home/dyuthi/prog2/P2-Image-Effects-Project/ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/service/logs");
+        try{
+            PrintWriter writer = new PrintWriter(file);
+            writer.print("");
+            writer.close();
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
         return;
     }
 
